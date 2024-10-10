99cc4f565792a77e7d863f6ed43480d9dc96c25e
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-10-10 15:34:52 +0530

    data updates

--- app/projects/index.tsx
+++ app/projects/index.tsx
@@ -1,20 +1,14 @@
-"use client";
-
 import ProjectCard from "@/components/ProjectCard";
 import projectList from "@/lib/projects.json";
-import { useState } from "react";
 
 const Projects = () => {
-    const [showAll, setShowAll] = useState(false);
-    let count = 6;
-    if (showAll) count = projectList.length;
     return (
         <div className="flex flex-col gap-6 mt-6 scroll-m-12" id="projects">
             <p className="text-center text-2xl underline decoration-cyan-400 decoration-2 underline-offset-8">
                 Projects
             </p>
             <div className="grid lg:grid-cols-1 justify-center flex-wrap gap-6">
-                {projectList.slice(0, count).map((o: any) => (
+                {projectList.map((o: any) => (
                     <ProjectCard
                         key={o.title}
                         title={o.title}
@@ -26,12 +20,6 @@ const Projects = () => {
                     />
                 ))}
             </div>
-            <button
-                onClick={() => setShowAll(!showAll)}
-                className="text-black my-2 bg-cyan-400 text-md px-3 py-2 w-fit self-center rounded"
-            >
-                {showAll ? "Collapse" : "View all"}
-            </button>
         </div>
     );
 };
